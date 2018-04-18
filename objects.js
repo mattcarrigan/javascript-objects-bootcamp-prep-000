07eda68073d2471adefb666953dd03e5c7c386ee
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-04-18 16:54:50 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -12,12 +12,6 @@ updatePlaylist (playlist,'My Bloody Valentine',"Sometimes")
 updatePlaylist (playlist,'Slowdive',"Alison")
 
 /*
-Hello! I can pass the final test using: delete playlist.Slowdive
-
-However, I can't make this work from inside a function. What needs to change in order to delete the key-value pair using the removeFromPlaylist function?
-
-Here is the full code:
-
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
   return playlist;
