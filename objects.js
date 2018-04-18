42043bcb3779b7a0b74a715bce2d880bb7de1b81
Matthew Carrigan <matthew.carrigan@gmail.com>
2018-04-18 16:15:38 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -12,6 +12,10 @@ updatePlaylist (playlist,'My Bloody Valentine',"Sometimes")
 updatePlaylist (playlist,'Slowdive',"Alison")
 
 /*
+Hello! I can pass the final test using: delete playlist.Slowdive
+
+the following code won't delete the key-value pair.:
+  
 function removeFromPlaylist(playlist, artistName){
   delete playlist.artistName;
   return playlist;
